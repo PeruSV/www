49016995086a5556f2cfc0cd7dc6549d75c87e60
elvitucho <el_vitucho@yahoo.com>
2016-10-01 23:22:56 -0700

    fixing header activa class

--- src/wp-content/themes/gateway/template-parts/content-top-menu.php
+++ src/wp-content/themes/gateway/template-parts/content-top-menu.php
@@ -1,22 +1,34 @@
+<?php $slug = basename(get_permalink()); 
+$class_home="";
+$class_mission="";
+$class_events="";
+if($slug == "mision") {
+  $class_mission = "active";
+} else if ($slug == "eventos-de-la-red") {
+  $class_events = "active";
+} else {
+  $class_home="active";
+}
+?>
 <nav class="psv-top-bar">
   <ul>
     <li>
-    <a href="/" class="active">
+    <a href="/" class="<?php echo $class_home;?>">
       perusv
     </a>
     </li>
     <li>
-      <a href="index.php/mision/">
+      <a href="index.php/mision/" class="<?php echo $class_mission;?>">
         Mision
       </a>
     </li>
     <li>
-      <a href="index.php/eventos-de-la-red/">
+      <a href="index.php/eventos-de-la-red/" class="<?php echo $class_events;?>">
         Eventos
       </a>
     </li>
     <li>
-      <a href="#">
+      <a href="mailto:contact@perusv.com?Subject=Perusv%20Contact" target="_top">
         Contacto
       </a>
     </li>
