a6beac9661a91fd5ee5f0b5057e3662bfa457685
elvitucho <el_vitucho@yahoo.com>
2016-10-15 12:55:33 -0700

    filtering home page by Main category

--- src/wp-content/themes/gateway/index.php
+++ src/wp-content/themes/gateway/index.php
@@ -13,13 +13,13 @@ get_header(); ?>
 		<div class="large-8 columns">
 
 			<main id="main" class="site-main" role="main">
-
+			<?php query_posts('category_name=Main');?>			
 			<?php if ( have_posts() ) : ?>
 
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
+					<?php 
 						/* Include the Post-Format-specific template for the content.
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
